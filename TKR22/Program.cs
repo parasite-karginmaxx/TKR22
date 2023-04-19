c3cf7c36284f0f3df5511fd9338e14dac76e363f
Kargin Maxim <karginmaxx@gmail.com>
2023-04-19 16:00:40 +0300

    Add project files.

---
+++ TKR22/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace TKR22
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            dynamic Theme = new System.Dynamic.ExpandoObject();
+            Console.WriteLine("Введите название аудитории: ");
+            Theme.Name = Console.ReadLine() ?? @"Тема не указана";
+
+
+        }
+    }
+}
